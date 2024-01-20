e0836f529371010377e9d53ec5c39421abcd5c65
Lucas <lucas.alazary@ecole-isitech.fr>
2024-01-20 16:26:46 +0100

    incompatibilité est en cours

--- MedicamentInfo.Designer.cs
+++ MedicamentInfo.Designer.cs
@@ -28,75 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btnmod = new Button();
             txtmed = new Label();
             label2 = new Label();
             label3 = new Label();
             txtContreIndication = new TextBox();
             txtLibelleMed = new TextBox();
+            cmbAntecedents = new ComboBox();
+            cmbAllergies = new ComboBox();
+            cmbMedicaments = new ComboBox();
+            btnCREERinc = new Button();
             SuspendLayout();
             // 
-            // button1
+            // btnmod
             // 
-            button1.Location = new Point(121, 346);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnmod.Location = new Point(12, 213);
+            btnmod.Margin = new Padding(3, 2, 3, 2);
+            btnmod.Name = "btnmod";
+            btnmod.Size = new Size(82, 22);
+            btnmod.TabIndex = 0;
+            btnmod.Text = "button1";
+            btnmod.UseVisualStyleBackColor = true;
+            btnmod.Click += btnmod_Click;
             // 
             // txtmed
             // 
             txtmed.AutoSize = true;
-            txtmed.Location = new Point(159, 21);
+            txtmed.Location = new Point(33, 9);
             txtmed.Name = "txtmed";
-            txtmed.Size = new Size(50, 20);
+            txtmed.Size = new Size(38, 15);
             txtmed.TabIndex = 1;
             txtmed.Text = "label1";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 114);
+            label2.Location = new Point(12, 41);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(38, 15);
             label2.TabIndex = 2;
             label2.Text = "label2";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(51, 245);
+            label3.Location = new Point(22, 138);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(38, 15);
             label3.TabIndex = 3;
             label3.Text = "label3";
             // 
             // txtContreIndication
             // 
-            txtContreIndication.Location = new Point(196, 264);
+            txtContreIndication.Location = new Point(1, 165);
+            txtContreIndication.Margin = new Padding(3, 2, 3, 2);
             txtContreIndication.Name = "txtContreIndication";
-            txtContreIndication.Size = new Size(125, 27);
+            txtContreIndication.Size = new Size(110, 23);
             txtContreIndication.TabIndex = 4;
             // 
             // txtLibelleMed
             // 
-            txtLibelleMed.Location = new Point(26, 150);
+            txtLibelleMed.Location = new Point(12, 67);
+            txtLibelleMed.Margin = new Padding(3, 2, 3, 2);
             txtLibelleMed.Name = "txtLibelleMed";
-            txtLibelleMed.Size = new Size(125, 27);
+            txtLibelleMed.Size = new Size(110, 23);
             txtLibelleMed.TabIndex = 5;
+            // 
+            // cmbAntecedents
+            // 
+            cmbAntecedents.FormattingEnabled = true;
+            cmbAntecedents.Location = new Point(253, 198);
+            cmbAntecedents.Name = "cmbAntecedents";
+            cmbAntecedents.Size = new Size(323, 23);
+            cmbAntecedents.TabIndex = 6;
+            // 
+            // cmbAllergies
+            // 
+            cmbAllergies.FormattingEnabled = true;
+            cmbAllergies.Location = new Point(277, 151);
+            cmbAllergies.Name = "cmbAllergies";
+            cmbAllergies.Size = new Size(343, 23);
+            cmbAllergies.TabIndex = 7;
+            // 
+            // cmbMedicaments
+            // 
+            cmbMedicaments.FormattingEnabled = true;
+            cmbMedicaments.Location = new Point(277, 95);
+            cmbMedicaments.Name = "cmbMedicaments";
+            cmbMedicaments.Size = new Size(299, 23);
+            cmbMedicaments.TabIndex = 8;
+            // 
+            // btnCREERinc
+            // 
+            btnCREERinc.Location = new Point(292, 267);
+            btnCREERinc.Name = "btnCREERinc";
+            btnCREERinc.Size = new Size(75, 23);
+            btnCREERinc.TabIndex = 9;
+            btnCREERinc.Text = "button1";
+            btnCREERinc.UseVisualStyleBackColor = true;
+            btnCREERinc.Click += btnCREERinc_Click;
             // 
             // MedicamentInfo
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(btnCREERinc);
+            Controls.Add(cmbMedicaments);
+            Controls.Add(cmbAllergies);
+            Controls.Add(cmbAntecedents);
             Controls.Add(txtLibelleMed);
             Controls.Add(txtContreIndication);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(txtmed);
-            Controls.Add(button1);
+            Controls.Add(btnmod);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "MedicamentInfo";
             Text = "MedicamentInfo";
             ResumeLayout(false);
@@ -105,11 +152,15 @@
 
         #endregion
 
-        private Button button1;
+        private Button btnmod;
         private Label txtmed;
         private Label label2;
         private Label label3;
         private TextBox txtContreIndication;
         private TextBox txtLibelleMed;
+        private ComboBox cmbAntecedents;
+        private ComboBox cmbAllergies;
+        private ComboBox cmbMedicaments;
+        private Button btnCREERinc;
     }
 }
--- MedicamentInfo.cs
+++ MedicamentInfo.cs
@@ -1,18 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ZZinventory
 {
     public partial class MedicamentInfo : Form
     {
         private string medicamentId;
+        private string connectionString;
+
+        public class MedicamentItem
+        {
+            public int Id { get; set; }
+            public string Libelle { get; set; }
+
+            public MedicamentItem(int id, string libelle)
+            {
+                Id = id;
+                Libelle = libelle;
+            }
+
+            public override string ToString()
+            {
+                return Libelle;
+            }
+        }
+
+        public class AntecedentItem
+        {
+            public int Id { get; set; }
+            public string Libelle { get; set; }
+
+            public AntecedentItem(int id, string libelle)
+            {
+                Id = id;
+                Libelle = libelle;
+            }
+
+            public override string ToString()
+            {
+                return Libelle;
+            }
+        }
+
+        public class AllergieItem
+        {
+            public int Id { get; set; }
+            public string Libelle { get; set; }
+
+            public AllergieItem(int id, string libelle)
+            {
+                Id = id;
+                Libelle = libelle;
+            }
+
+            public override string ToString()
+            {
+                return Libelle;
+            }
+        }
 
         public MedicamentInfo(string id, string libelle, string contreIndication)
         {
@@ -20,8 +67,149 @@ namespace ZZinventory
             medicamentId = id;
             txtLibelleMed.Text = libelle;
             txtContreIndication.Text = contreIndication;
-            txtmed.Text = libelle + " - " + contreIndication;
+            connectionString = ConfigurationManager.ConnectionStrings["localhost"].ConnectionString;
+            RemplirComboBoxMedicaments();
+            RemplirComboBoxAntecedents();
+            RemplirComboBoxAllergies();
         }
 
+        private void CreerIncompatibilite(int idAntecedent, int idAllergie, int idMedicament)
+        {
+            string query = "INSERT INTO incompatible (id_a, id_al, id_med) VALUES (@IdAntecedent, @IdAllergie, @IdMedicament)";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdAntecedent", idAntecedent);
+                    command.Parameters.AddWithValue("@IdAllergie", idAllergie);
+                    command.Parameters.AddWithValue("@IdMedicament", idMedicament);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            MessageBox.Show("L'incompatibilité a été créée avec succès.");
+        }
+
+        private void RemplirComboBoxMedicaments()
+        {
+            string query = "SELECT id_med, libelle_med FROM Medicament";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int idMedicament = reader.GetInt32("id_med");
+                        string libelleMedicament = reader.GetString("libelle_med");
+                        cmbMedicaments.Items.Add(new MedicamentItem(idMedicament, libelleMedicament));
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        private void RemplirComboBoxAntecedents()
+        {
+            string query = "SELECT id_a, libelle_a FROM Antecedent";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int idAntecedent = reader.GetInt32("id_a");
+                        string libelleAntecedent = reader.GetString("libelle_a");
+                        cmbAntecedents.Items.Add(new AntecedentItem(idAntecedent, libelleAntecedent));
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        private void RemplirComboBoxAllergies()
+        {
+            string query = "SELECT id_al, libelle_al FROM Allergie";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int idAllergie = reader.GetInt32("id_al");
+                        string libelleAllergie = reader.GetString("libelle_al");
+                        cmbAllergies.Items.Add(new AllergieItem(idAllergie, libelleAllergie));
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        private void btnmod_Click(object sender, EventArgs e)
+        {
+            string nouveauLibelle = txtLibelleMed.Text;
+            string nouvelleContreIndication = txtContreIndication.Text;
+
+            string query = "UPDATE Medicament SET libelle_med = @Libelle, contre_indication = @ContreIndication WHERE id_med = @Id";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Libelle", nouveauLibelle);
+                    command.Parameters.AddWithValue("@ContreIndication", nouvelleContreIndication);
+                    command.Parameters.AddWithValue("@Id", medicamentId);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Les informations du médicament ont été modifiées avec succès dans la base de données.");
+        }
+
+        private void btnCREERinc_Click(object sender, EventArgs e)
+        {
+            if (cmbMedicaments.SelectedItem == null)
+            {
+                MessageBox.Show("Veuillez sélectionner un médicament.");
+                return;
+            }
+
+            bool hasAntecedent = cmbAntecedents.SelectedItem != null;
+            bool hasAllergie = cmbAllergies.SelectedItem != null;
+
+            if (!hasAntecedent && !hasAllergie)
+            {
+                MessageBox.Show("Veuillez sélectionner au moins un antécédent ou une allergie.");
+                return;
+            }
+
+            MedicamentItem selectedMedicament = (MedicamentItem)cmbMedicaments.SelectedItem;
+            int idMedicament = selectedMedicament.Id;
+
+            int? idAntecedent = null;
+            if (hasAntecedent)
+            {
+                AntecedentItem selectedAntecedent = (AntecedentItem)cmbAntecedents.SelectedItem;
+                idAntecedent = selectedAntecedent.Id;
+            }
+
+            int? idAllergie = null;
+            if (hasAllergie)
+            {
+                AllergieItem selectedAllergie = (AllergieItem)cmbAllergies.SelectedItem;
+                idAllergie = selectedAllergie.Id;
+            }
+
+            CreerIncompatibilite(idAntecedent.GetValueOrDefault(), idAllergie.GetValueOrDefault(), idMedicament);
+
+            MessageBox.Show("L'incompatibilité a été créée avec succès.");
+        }
     }
 }
